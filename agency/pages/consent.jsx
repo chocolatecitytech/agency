34930c553126b92f4161ee9f9eabea0bbb4c7b70
chocolatecitytech <68204094+chocolatecitytech@users.noreply.github.com>
2020-10-29 22:09:28 -0400

    Features/consent page (#2)
    * added consent page
    
    * adding consent. adding component
    
    Co-authored-by: derick bonsu <chocolatecitytech.com>

---
+++ agency/pages/consent.jsx
@@ -0,0 +1,23 @@
+function consent() {
+    return (
+        <div>
+           The researcher requests your consent for participation in a study about ___. This consent form asks you to allow the researcher to record and view the interview and to use your comments to enhance understanding of the topic. The form also asks your permission to use related observations, images or posts as data in this study.
+
+This questionnaire asks for your preferences about whether to remain anonymous or to allow the researcher to name you and your [school/organization/business] and to quote you directly. It also asks you to indicate your preferences for use of visual images shared or generated in the interview.
+
+
+Participation in this study is completely voluntary. If you decide not to participate there will not be any negative consequences. Please be aware that if you decide to participate, you may stop participating at any time and you may decide not to answer any specific question.
+
+The researcher will maintain the confidentiality of the research records or data, and all data will be destroyed in [state time frame.]
+
+By submitting this form you are indicating that you have read the description of the study, are over the age of 18, and that you agree to the terms as described.
+
+
+If you have any questions, or would like a copy of this consent letter, please contact me at ---.
+
+Thank you in advance for your participation!
+        </div>
+    )
+}
+
+export default consent
